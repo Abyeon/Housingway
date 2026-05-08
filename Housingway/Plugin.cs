35bc435919c36392771edf0381aa7c820d938629
Abyeon <wpseals@gmail.com>
2026-05-07 23:35:39 -0400

    Initial commit

---
+++ Housingway/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using Housingway.Windows;
+
+namespace Housingway;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+    private const string CommandName = "/housingway";
+
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("Housingway");
+    private ConfigWindow ConfigWindow { get; init; }
+
+    public Plugin()
+    {
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        
+        ConfigWindow = new ConfigWindow(this);
+
+        WindowSystem.AddWindow(ConfigWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Show the Housingway config window."
+        });
+        
+        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
+    }
+
+    public void Dispose()
+    {
+        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
+        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
+        
+        WindowSystem.RemoveAllWindows();
+
+        ConfigWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        // In response to the slash command, toggle the display status of our main ui
+        ConfigWindow.Toggle();
+    }
+    
+    public void ToggleConfigUi() => ConfigWindow.Toggle();
+}
