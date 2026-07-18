a3bfd720c5971ed72d1e08f961cf20b84a970f6f
Abyeon <17396444+Abyeon@users.noreply.github.com>
2026-07-18 18:49:55 -0400

    Add IndoorSun.cs tweak

---
+++ Housingway/Tweaks/IndoorSun.cs
@@ -0,0 +1,43 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
+using Housingway.Utils;
+
+namespace Housingway.Tweaks;
+
+public class IndoorSun : BaseTweak
+{
+    public override string Name { get; init; } = "Indoor Sun";
+    public override string Description { get; init; } = "Enables the Sun when indoors!";
+    public override void Enable()
+    {
+        HousingService.OnEnterHousingArea += OnEnterHousingArea;
+
+        if (HousingService.IsInside)
+        {
+            ToggleSun();
+        }
+    }
+
+    private void OnEnterHousingArea(bool indoors)
+    {
+        if (!indoors) return;
+        ToggleSun();
+    }
+
+    private static unsafe void ToggleSun(bool indoor = false)
+    {
+        var graphics = GraphicsConfig.Instance();
+        graphics->IsIndoor = indoor;
+    }
+
+    public override void Disable()
+    {
+        HousingService.OnEnterHousingArea -= OnEnterHousingArea;
+        
+        if (HousingService.IsInside)
+        {
+            ToggleSun(true);
+        }
+    }
+
+    public override void Dispose() { }
+}
--- roadmap.md
+++ roadmap.md
@@ -4,14 +4,14 @@ List of goal tweaks to include with Housingway
 ---
 
 1. [X] Skybox Editor
-2. [ ] Toggle for Void Lighting
+2. [X] Toggle for Void Lighting
 3. [x] Interior Lighting slider
 4. [x] SSAO+ toggle
 5. [X] Cast shadows toggle
 6. [ ] Remove void sun (maybe?)
 7. [ ] Phasing bounding sphere display
 8. [ ] Phasing toggle
-9. [ ] Include cull fix (and check if HousingCullFix exists in plugins before applying fix)
+9. [ ] ~~Include cull fix (and check if HousingCullFix exists in plugins before applying fix)~~ (irrelevant until SE enables culling again)
 10. [x] Disable housing shell "lightgard.mdl"
 11. [x] Disable shame cube "env_cube.mdl"
 12. [x] Disable camera collision with furnishings
